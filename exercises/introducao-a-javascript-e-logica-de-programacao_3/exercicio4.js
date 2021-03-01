dd623a17fd6756ef0ed1bd7c0908ef25d764d067
Junior030 <juniorhenrique030@gmail.com>
2021-03-01 14:55:38 -0300

    Exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio4.js
@@ -1,15 +1,16 @@
 let n = 5, x = 3;
 let asterisco = "*";
+let aux = " ";
 
 
 for (let i = 1; i <= n; i++) {    
     for (let j = 1; j <= n; j++) { 
-        if( i == x){
-            
+        if(i = 5){
+            aux += asterisco;
         }else{
             aux = aux + " ";
         }    
     }
     console.log(aux); 
-    aux = " ";   
+      
 }
