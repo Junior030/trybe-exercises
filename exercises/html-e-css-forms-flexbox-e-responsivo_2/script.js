57d394010cb3991036a71f62ee4fe00eee48d34a
Junior030 <juniorhenrique030@gmail.com>
2021-03-18 15:03:02 -0300

    Exercicio frameworks bootstrap

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
@@ -0,0 +1,41 @@
+const estadosBR = {
+    AC: 'Acre',
+    AL: 'Alagoas',
+    AP: 'Amapá',
+    AM: 'Amazonas',
+    BA: 'Bahia',
+    CE: 'Ceará',
+    DF: 'Distrito Federal',
+    ES: 'Espírito Santo',
+    GO: 'Goiás',
+    MA: 'Maranhão',
+    MT: 'Mato Grosso',
+    MS: 'Mato Grosso do Sul',
+    MG: 'Minas Gerais',
+    PA: 'Pará',
+    PB: 'Paraíba',
+    PR: 'Paraná',
+    PE: 'Pernambuco',
+    PI: 'Piauí',
+    RJ: 'Rio de Janeiro',
+    RN: 'Rio Grande do Norte',
+    RS: 'Rio Grande do Sul',
+    RO: 'Rondônia',
+    RR: 'Roraima',
+    SC: 'Santa Catarina',
+    SP: 'São Paulo',
+    SE: 'Sergipe',
+    TO: 'Tocantins',
+};
+
+function optionsEstadosBR(estadosBR) {
+    const estados =  document.querySelector('#estados');
+    for (const key in estadosBR) {
+        let criando = document.createElement('option');
+        criando.value = key;
+        criando.innerText = estadosBR[key];
+        estados.appendChild(criando);
+    }
+}
+
+optionsEstadosBR(estadosBR);
