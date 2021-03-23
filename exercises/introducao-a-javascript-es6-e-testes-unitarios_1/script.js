7bed838e1bfdfd592490591e3c2d1e69925de053
Junior030 <juniorhenrique030@gmail.com>
2021-03-23 15:55:06 -0300

    JavaScript ES6

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -0,0 +1,18 @@
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+//   testingScope(true);
+
+
+  const testingScope = (escopo) => (escopo === true) ? ' ótimo, fui utilizada no escopo !': 'Não devo ser utilizada fora meu escopo (else)'; 
+
+  console.log(testingScope(true));
